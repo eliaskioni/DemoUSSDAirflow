36b82c8f24bb4e42b41d27fbb0ccffdbffb13708
mwas <francismwangi152@gmail.com>
2017-01-25 14:54:46 +0300

    refactored for the project to run

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --noinput
 
 python manage.py validate_ussd_journey ./core/customer_journey.yml
 
-exec  gunicorn --bind=0.0.0.0:80 config.wsgi \
+exec  gunicorn --bind=0.0.0.0:80 AirflowUssdDemo.wsgi \
         --workers=5\
         --log-level=info \
         --log-file=-\
